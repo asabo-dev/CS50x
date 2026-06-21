876aef4ffb6c549838c2ad05eaa3fba793757c6d
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-06-21 15:14:27 +0800

    How to use 'help50' to understand Error Message'

---
+++ Week 1 - C/Lecture/code_snipets/help.c
@@ -0,0 +1,15 @@
+int main(void)
+{
+    printf("hello, world\n");
+}
+
+/*
+Assumming '#include <stdio.h>' line is omitted
+An Error Message will pop
+Using the command "help50 make help" will point out the error/solution.
+Example:
+help.c:3:5: note: include the header <stdio.h> or explicitly provide a declaration for 'printf'
+1 error generated.
+make: *** [<builtin>: help] Error 1
+
+*/
