a8eb65d9c7b526a97fc5947ef5f73708c0216b4a
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-06-21 15:30:48 +0800

    How to use 'style50' for good looking source code

---
+++ Week 1 - C/Lecture/code_snipets/styling.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    // Greet user
+    printf("hello, world\n");
+}
+
+/*
+Terminal Command:
+running 'style50 style.c'
+Would suggest ways you can improve the style of your source code.
+Such as adding comments
+*/
