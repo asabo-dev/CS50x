009f9c99a86375925da31f6ca8825fed814f7969
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-06-21 13:55:24 +0800

    Print 'hello, world' in C

---
+++ Week 1 - C/Lecture/code_snipets/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
+
+
+/*
+Terminal Commands
+$ make helo -- compile source code
+$ ./helo -- call function print() to execute source code 
+hello, world
+$
+*/
