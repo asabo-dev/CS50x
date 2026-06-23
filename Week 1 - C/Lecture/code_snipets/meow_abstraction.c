7b664a5ea61f212699fde93da317d30f0ed55859
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-06-23 22:46:39 +0800

    Abstraction example.

---
+++ Week 1 - C/Lecture/code_snipets/meow_abstraction.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Prototype
+void meow(void);
+
+int main(void)
+{
+    for (int i = 0; i < 7; i++)
+    {
+        meow();
+    }
+}
+
+void meow(void)
+{
+    printf("meow\n");
+}
+
+/*
+- Prototype (snapshot) of the function is placed at the top to alert the program that such a functions exists later in the program.
+- Create a function called 'meow'.
+- You can call the function within your code.
+
+Terminal Output
+$ make meow
+$ ./meow
+meow
+meow
+meow
+meow
+meow
+meow
+meow
+$ 
+*/
