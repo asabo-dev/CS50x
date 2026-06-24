85cc48693cef4f2d46595e09a2b531d279f90ae0
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-06-24 12:22:10 +0800

    Use 'for' loop to Print '?' as represented in Super Mario .

---
+++ Week 1 - C/Lecture/code_snipets/mario_II.c
@@ -0,0 +1,21 @@
+// Print out the question mark boxes in Mario game using a 'for' loop.
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    {
+        printf("\n");
+    }
+}
+
+/*
+Terminal Output
+$ make mario
+$ ./mario
+????
+$ 
+*/
