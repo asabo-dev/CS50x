e1ea772a7ca71c061d3dba030cf98b33c022a449
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-07-10 15:48:21 +0800

    Prompt user exercise.

---
+++ Week 1 - C/Exercises/me/hello.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for his/her name.
+    string name = get_string("What is your name?\n");
+    // Print output.
+    printf("hello, %s\n", name);
+}
