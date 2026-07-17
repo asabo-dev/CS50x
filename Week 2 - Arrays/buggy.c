228eb45de2cfcc720e619b6bf719843c569222fc
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-07-17 12:23:55 +0800

    Highlight bug for missing library

--- Week 2 - Arrays/buggy.c
+++ Week 2 - Arrays/buggy.c
@@ -2,15 +2,18 @@
 
 int main(void)
 {
-    name = get_string("What's your name? ");
+    string name = get_string("What's your name? ");
     printf("Hello, world!\n");
 }
 
 /*
 Terminal Output
 $ make buggy
-buggy.c:5:5: error: use of undeclared identifier 'name'
-    5 |     name = get_string("What's your name? ");
+buggy.c:5:5: error: use of undeclared identifier 'string'; did you mean 'stdin'?
+    5 |     string name = get_string("What's your name? ");
+      |     ^~~~~~
+      |     stdin
 
-Output shows that there's a bug on line 5; the variable 'name' was not given a type. 
+Output shows that there's a bug on line 5,
+because the proper library (cs50) was not imported. 
     */
