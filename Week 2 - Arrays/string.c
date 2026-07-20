8b56a59c0c64bc461d7056aa4fc5aaeee4846978
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-07-20 12:38:23 +0800

    Print a simple string

---
+++ Week 2 - Arrays/string.c
@@ -0,0 +1,17 @@
+// Print a simple string
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = "HI!";
+    printf("%s\n", s);
+}
+
+/*
+Terminal Output
+$ make string
+$ ./string
+HI!
+$
+*/
