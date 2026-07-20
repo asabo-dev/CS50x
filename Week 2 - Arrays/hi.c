a6b022383193c40bd58a1e4089026ec8756a7642
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-07-20 11:55:11 +0800

    Print simple 'char' characters.

---
+++ Week 2 - Arrays/hi.c
@@ -0,0 +1,19 @@
+// Poor design example
+#include <stdio.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'i';
+    char c3 = '!';
+
+    printf("%c%c%c\n", c1, c2, c3);
+}
+
+/*
+Terminal Output
+$ make hi
+$ ./hi
+Hi!
+$ 
+*/
