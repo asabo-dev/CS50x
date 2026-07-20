92c181027e9557aed5b16149baebb1892ca41cf8
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-07-20 12:42:57 +0800

    Print a simple string as 'char' characters

--- Week 2 - Arrays/string.c
+++ Week 2 - Arrays/string.c
@@ -1,11 +1,11 @@
-// Print a simple string
+// Print a simple string and 'char' characters
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     string s = "HI!";
-    printf("%s\n", s);
+    printf("%c%c%c\n", s[0], s[1], s[2]);
 }
 
 /*
