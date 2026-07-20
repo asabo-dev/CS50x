cedc8fb9b41df2433b1c0d58b02b2108f195cd24
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-07-20 12:46:35 +0800

    Print a simple string as 'int' type

--- Week 2 - Arrays/string.c
+++ Week 2 - Arrays/string.c
@@ -1,17 +1,17 @@
-// Print a simple string and 'char' characters
+// Print a simple string as 'int' type
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     string s = "HI!";
-    printf("%c%c%c\n", s[0], s[1], s[2]);
+    printf("%i %i %i\n", s[0], s[1], s[2]);
 }
 
 /*
 Terminal Output
 $ make string
 $ ./string
-HI!
+72 73 33
 $
 */
